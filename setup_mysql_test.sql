2dccfaf9ce96fe2bca50c631b1004577d4770a34
Ore1232 <oreoluwa59keripe@gmail.com>
2023-04-19 19:21:32 -0700

    python

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,15 @@
+SET @db_name := 'hbnb_test_db';
+SET @user_name := 'hbnb_test';
+SET @user_pass := 'hbnb_test_pwd';
+
 PREPARE stmt FROM '
-    CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-    CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY "hbnb_test_pwd";
-    GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO hbnb_test@localhost;
-    GRANT SELECT ON `performance_schema`.* TO hbnb_test@localhost;
+    CREATE DATABASE IF NOT EXISTS ?;
+    CREATE USER IF NOT EXISTS ?@localhost IDENTIFIED BY ?;
+    GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO ?@localhost;
+    GRANT SELECT ON `performance_schema`.* TO ?@localhost;
     FLUSH PRIVILEGES;
 ';
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
 
+EXECUTE stmt USING @db_name, @user_name, @user_pass, @user_name, @user_name;
+
+DEALLOCATE PREPARE stmt;
